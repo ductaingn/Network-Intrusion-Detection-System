a88dc154e94b75147d79c907d18a553f6427397a
R4C00n <superc0605@gmail.com>
2025-01-01 20:36:05 +0700

    Added new feature: write classified data to kafka topic

---
+++ client/utilities.py
@@ -0,0 +1,19 @@
+import json
+import socket
+
+def load_config(config_file):
+    with open(config_file, 'r') as file:
+        return json.load(file)
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+    
